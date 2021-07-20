95ec7a598d97d1a08365ddae9181be85a70feafa
Mikhail Ivakhnenko <mihail.kursk@mail.ru>
2021-07-20 12:43:02 +0300

    Main HW

---
+++ HW_7/HW_7/Matrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_7
+{
+    class Matrix
+    {
+        public int[,] MatrixArr { get; set; }
+
+
+        public Matrix(int i, int j)
+        {
+            MatrixArr = new int[i, j];
+        }
+
+        public int[,] VariantsWithRecursion(int i, int j)
+        {
+            if (i >= MatrixArr.GetLength(0) || j >= MatrixArr.GetLength(1))
+                return MatrixArr;
+
+            MatrixArr[i, j] += 1;
+
+            MatrixArr = VariantsWithRecursion(i + 1, j);
+            MatrixArr = VariantsWithRecursion(i, j + 1);
+
+            return MatrixArr;
+
+        }
+
+        public int[,] VariantsWithSum()
+        {
+            for (int i = 0; i < MatrixArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < MatrixArr.GetLength(1); j++)
+                {
+                    if (i - 1 >= 0)
+                    {
+                        MatrixArr[i, j] += MatrixArr[i - 1, j];
+                    }
+
+                    if (j - 1 >= 0)
+                    {
+                        MatrixArr[i, j] += MatrixArr[i, j - 1];
+                    }
+                }
+            }
+            return MatrixArr;
+        }
+    }
+}
---
+++ HW_7/HW_7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HW_7
+{
+    class Program
+    {
+        // Задание
+        // Для прямоугольного поля размера M на N клеток, подсчитать количество путей из верхней левой клетки в правую нижнюю. 
+        // Известно что ходить можно только на одну клетку вправо или вниз. 
+        static void Main(string[] args)
+        {
+            int n = 10; // строки
+            int m = 8; // столбцы
+
+
+            // Вариант с рекурсией
+            Console.WriteLine("Вариант с рекурсией");
+            Matrix matrix = new Matrix(n, m);
+            matrix.VariantsWithRecursion(0, 0);
+
+            string res = null;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    res += matrix.MatrixArr[i, j].ToString() + "\t";
+                }
+                Console.WriteLine(res);
+                res = "";
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Вариант со сложением");
+            Matrix matrix_2 = new Matrix(n, m);
+            matrix_2.MatrixArr[0, 0] = 1;
+
+            matrix_2.VariantsWithSum();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    res += matrix_2.MatrixArr[i, j].ToString() + "\t";
+                }
+                Console.WriteLine(res);
+                res = "";
+            }
+        }
+    }
+}
