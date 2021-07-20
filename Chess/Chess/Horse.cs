cb43a8067cd824f2c2ba21f1ff97825a3cbc6491
Mikhail <mihail.kursk@mail.ru>
2021-07-21 01:04:41 +0300

    Minor changes

--- Chess/Chess/Horse.cs
+++ Chess/Chess/Horse.cs
@@ -10,7 +10,7 @@ namespace Chess
         public int Y { get; set; }
 
         public static int MaxTurns;
-        public static int Retry;
+        public static long Retry;
 
         public struct Move
         {
@@ -32,14 +32,61 @@ namespace Chess
                 this.Moves = moves;
             }
 
-            public void DisplayPossition()
+            public void PrintPossition()
             {
                 Console.WriteLine($"X = {X}, Y = {Y}");
             }
 
-            public void DisplayPossitionAndMoves()
+            public void PrintPossitionAndMoves()
             {
                 Console.WriteLine($"X = {X}, Y = {Y}, Moves = {Moves}");
+            }
+
+            public static void DisplayPossitionAndMoves(Board board, List<Move> moves)
+            {
+                int x = board.X;
+                int y = board.Y;
+
+                char[,] marks = new char[x, y];
+
+                foreach (Move move in moves)
+                {
+                    Console.Clear();
+
+                    marks[move.X - 1, move.Y - 1] = 'К';
+
+                    // Рисуем доску
+
+                    for (int i = 0; i < y; i++)
+                    {
+                        Console.WriteLine($"{new string('-', x * 4 + 1)}");
+                        for (int j = 0; j < x; j++)
+                        {
+                            Console.Write($"|   ");
+                        }
+                        Console.Write('|');
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine($"{new string('-', x * 4 + 1)}");
+
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int j = 0; j < y; j++)
+                        {
+                            Console.SetCursorPosition(i * 4 + 2, j * 2 + 1);
+                            Console.Write($"{marks[i, j]}");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.ReadLine();
+                    Console.SetCursorPosition((move.X - 1) * 4 + 2, (move.Y - 1) * 2 + 1);
+                    marks[move.X - 1, move.Y - 1] = 'x';
+                    Console.Write($"{marks[move.X - 1, move.Y - 1]}");
+
+                }
+
+                Console.SetCursorPosition(0, board.Y * 2 + 2);
+
             }
         }
 
@@ -202,36 +249,49 @@ namespace Chess
             return new Move(x, y, moves + 1);
         }
 
-        public List<Move> TryToFindMovementsWithAllPositions(Board board, List<Move> moves, List<Move> disabledCells, int i = 1)
+        public List<Move> TryToFindMovementsWithAllPositions(Board board, List<Move> moves, List<Move> disabledCells, ref int i)
         {
-            
+
             List<Move> possibleMoves = new List<Move>();
             bool display = false;
 
+            // Определяем возможные шаги
             possibleMoves = PossibleMovements(board, X, Y, disabledCells);
 
             foreach (Move move in possibleMoves)
             {
+                // Подсчет статистики и вывод на экран, чтобы не было скучно ждать
                 Retry++;
-
-                if (Retry % 1000000 == 0)
+                if (Retry % 1_000_000 == 0)
                 {
                     Console.WriteLine($"Attempts =  {Retry}, best result = {MaxTurns}, current result = {i}");
                 }
 
+                if (Retry > 100_000_000)
+                {
+                    MaxTurns = 0;
+                    return null;
+                }
+
+                // Запоминаем текущую позицию, чтобы можно было вернуться
                 int x = X;
                 int y = Y;
 
+                // Меняем позицию
                 Move newPossition = ChangePossition(board, move.X, move.Y, i);
                 i = newPossition.Moves;
 
+                // Также подсчет статистики
                 if (i > MaxTurns)
                     MaxTurns = i;
 
+                // Записываем сделанные ходы
                 moves.Add(newPossition);
 
+                // Маркируем пройденные клетки
                 disabledCells.Add(newPossition);
 
+                // Проверка на окончание поиска
                 if (i == board.X * board.Y)
                 {
                     Console.WriteLine("решение найдено");
@@ -239,19 +299,31 @@ namespace Chess
                     break;
                 }
 
-                TryToFindMovementsWithAllPositions(board, moves, disabledCells, i);
+                // Рекурсивный вызов после хода
+                TryToFindMovementsWithAllPositions(board, moves, disabledCells, ref i);
+
+                // Остановка поиска, если решение было найдено
+                if (i == board.X * board.Y)
+                    return moves;
+
+                // Откат хода
                 moves.RemoveAt(moves.Count - 1);
                 i--;
                 ChangePossition(board, x, y, i - 1);
                 disabledCells.RemoveAt(disabledCells.Count - 1);
             }
 
+            // Вывод решения
             if (display)
             {
                 foreach (Move move in moves)
                 {
-                    move.DisplayPossitionAndMoves();
+                    move.PrintPossitionAndMoves();
                 }
+
+                Console.WriteLine("Отрисовать? y/n");
+                if (Console.ReadLine().ToLower() == "y")
+                    Move.DisplayPossitionAndMoves(board, moves);
             }
 
             return moves;
--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -7,32 +7,51 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            int x = 7;
-            int y = 7;
+            int x = 8; // Ширина доски
+            int y = 8; // высота доски
+
+            // Для доски 5x5, хороший вариант 3 , 3
+            // Для доски 6x6, хороший вариант 3 , 3 
+            // Для доски 7x7, хороший вариант 2 , 2
+            // Для доски 
 
             Board board = new Board(x, y);
 
-            Horse horse = new Horse(4, 4);
+            Horse horse = new Horse(0, 0);
+            bool found = false;
 
-            /*List<Horse.Move> moves = horse.PossibleMovements(board, horse.X, horse.Y);
-
-            foreach (Horse.Move move in moves)
+            for (int z = 1; z <= x; z++)
             {
-                move.DisplayPossition();
-            }*/
+                for (int a = 1; a <= y; a++)
+                {
+                    horse.X = z;
+                    horse.Y = a;
 
-            Console.WriteLine();
+                    Console.WriteLine($"Проверяем конфигурацию X = {z}, Y = {a}");
+                    Horse.Retry = 0;
 
-            List<Horse.Move> disabledMoves = new List<Horse.Move>();
-            Horse.Move firstPossition = new Horse.Move(horse.X, horse.Y, 1);
-            disabledMoves.Add(firstPossition);
+                    // Вносим первую позицию в списки, чтобы конь не пытался на нее попасть
+                    List<Horse.Move> disabledMoves = new List<Horse.Move>();
+                    Horse.Move firstPossition = new Horse.Move(horse.X, horse.Y, 1);
+                    disabledMoves.Add(firstPossition);
 
-            List<Horse.Move> moves = new List<Horse.Move>();
-            moves.Add(firstPossition);
+                    List<Horse.Move> moves = new List<Horse.Move>();
+                    moves.Add(firstPossition);
 
-            horse.TryToFindMovementsWithAllPositions(board, moves, disabledMoves);
-            Console.WriteLine(Horse.MaxTurns);
-            Console.WriteLine(Horse.Retry);
+                    int i = 1;
+
+                    horse.TryToFindMovementsWithAllPositions(board, moves, disabledMoves, ref i);
+                    if (i == x * y)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                    break;
+            }
+            
+            Console.WriteLine($"Попыток = {Horse.Retry}");
 
         }
     }
